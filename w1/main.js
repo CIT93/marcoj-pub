632ed114f6feb5aff011f63df2dde65da9f4bc62
Makah101 <Marcobluednangel@gmail.com>
2025-01-28 02:31:02 +0000

    Week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log("Week 1 - Hello World");
+console.log("Hello from inside the main.js file");
+
+const myVar = 42;
+
+const myVarType = typeof(myVar); // is returned as a string
+
+console.log(myVarType);
+console.log(`myVarType ${myVarType}`);
+
+if(myVarType === "number") {
+    console.log(`this line will run`)
+} else {
+    console.log(`this line will not run`)
+}
+
+function runNow() {
+    console.log(`inside the function`);
+}
+
+runNow()
