78698a2b80786c19fc6b2e71bad81131ff0dfd0f
Makah101 <Marcobluednangel@gmail.com>
2025-02-27 10:55:17 +0000

    challenge 3

--- w7/render.js
+++ w7/render.js
@@ -1,8 +1,8 @@
 const TBL = document.getElementById("tab-data")
-let timesRun = 0  //needed so that renderTblRow keeps up as cfpData increases after first table is made
+let i = 0  //needed so that renderTblRow keeps up as cfpData increases after first table is made
 
 function renderTblHead () {
-    const headingTextArr = ["Name", "HouseHold", "HouseSize", "Footprint", "Actions"];
+    const headingTextArr = ["Name", "Total"];
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
     headingTextArr.forEach(function(text) {
@@ -15,46 +15,41 @@ function renderTblHead () {
 }
 
 //making row data ** Now with passed on values WOO! **
-function renderTblRow(data, fName, lName){
-    const fullName = `${fName} ${lName}`
-    const tempArr = [fullName, data.houseHoldMembers, data.houseSize, data.total]
+function renderTblRow(data, fName, lName, i){
+    i = parseInt(i)
+    const tempArr = [fName, data[i].total]
     const tr= document.createElement("tr")
     tempArr.forEach(function(text){
         const td = document.createElement("td");
         td.textContent = text;
         tr.appendChild(td);
     })
-    //adding buttons
-    const td = document.createElement("td")
-    const btnEdit = document.createElement("button")
-    btnEdit.textContent = "Edit";
-    const btnDel = document.createElement("button")
-    btnDel.textContent = "Del";
-    td.appendChild(btnEdit)
-    td.appendChild(btnDel)
-    tr.appendChild(td)
+    //adding buttons  --- removed for challenge
+//    const td = document.createElement("td")
+//    const btnEdit = document.createElement("button")
+//    btnEdit.textContent = "Edit";
+//    const btnDel = document.createElement("button")
+//    btnDel.textContent = "Del";
+//    td.appendChild(btnEdit)
+//    td.appendChild(btnDel)
+//    tr.appendChild(td)
     
     return tr;
 }
 
 
-/*challenge 2 could not figure out how to get the heading to only show up once
-the theory is create header or dont if made already.
-create the row for entered data (this would persist)
-create a new table by adding heading and row data
-I think what is throwing me off is that they have to be wrapped in the proper tags to make up the table.
+/* challenge 3
+was having trouble with using i to pass on a spicific array so passed on the i only to realize i had put it in the block scope
+this made it so each time the form was submitted it was reset to zero defeating the purpose. moved it to global. this allowed it to work
 */
  function renderTbl (data, fName, lName) {
     const table = document.createElement("table");
-    const table2 = document.createElement("table");
-    const table3 = document.createElement("table");
     const tbody = document.createElement("tbody");
-    let i = 0
     if (i===0) {
         const thead = renderTblHead();
         table.appendChild(thead);
     }
-    const trow = renderTblRow(data[i], fName, lName);
+    const trow = renderTblRow(data, fName, lName, i);
     tbody.appendChild(trow);
     table.appendChild(tbody);
     TBL.appendChild(table);
