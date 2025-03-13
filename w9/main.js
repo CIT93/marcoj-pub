d2f17b73381e4284974f745a1d06f8c21939ff56
Makah101 <Marcobluednangel@gmail.com>
2025-03-13 03:08:13 +0000

    w9 my attempt at the code, before I review other students code

--- w9/main.js
+++ w9/main.js
@@ -7,12 +7,31 @@ renderTbl(cfpData);
 
 FORM.addEventListener("submit", function(e){
     e.preventDefault();
+    const error = document.getElementById("error");
     const firstName = FORM.firstname.value;
     const lastName = FORM.lastname.value;
-    const NIH = parseInt(FORM.householdsize.value, 10);  //Had to convert the string to a number using a base10 number system
-    const HS = FORM.housesize.value;
-    start(NIH,HS,firstName, lastName);
-    saveLS(cfpData);
-    renderTbl(cfpData);
-    FORM.reset();
+    const message = []
+    let flag = 0
+    if (firstName === "") {
+        message.push("First name is required.");
+        flag++
+    } if (lastName === ""){
+        message.push(" Last name is required. ");
+        flag++
+    }
+    
+    if (flag === 0) {
+        const NIH = parseInt(FORM.householdsize.value, 10);  //Had to convert the string to a number using a base10 number system
+        const HS = FORM.housesize.value;
+        start(NIH,HS,firstName, lastName);
+        saveLS(cfpData);
+        renderTbl(cfpData);
+        FORM.reset();
+        flag = 0
+        error.innerText = ""
+    }else {
+        error.innerText = message
+        error.style.color = "red"
+    }
+    
 })
